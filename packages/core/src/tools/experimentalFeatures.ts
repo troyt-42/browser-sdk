3e226c442bd1bca9219bbacc794ac22107670dcb
Troy Tao <troy@faire.com>
2024-03-01 14:26:04 -0500

    Fix missing INP feature flag

--- packages/core/src/tools/experimentalFeatures.ts
+++ packages/core/src/tools/experimentalFeatures.ts
@@ -20,6 +20,7 @@ export enum ExperimentalFeature {
   WRITABLE_RESOURCE_GRAPHQL = 'writable_resource_graphql',
   TRACKING_CONSENT = 'tracking_consent',
   CUSTOM_VITALS = 'custom_vitals',
+  INTERACTION_TO_NEXT_PAINT = 'interaction_to_next_paint',
 }
 
 const enabledExperimentalFeatures: Set<ExperimentalFeature> = new Set()
